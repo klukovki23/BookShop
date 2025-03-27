fdd67df99bfa59b0ad46d0f63b73cfd90d781dcd
Kseniia <klukovki23@student.oulu.fi>
2025-03-27 16:19:28 +0200

    edit_books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,22 @@
 class BooksController < ApplicationController
     def index
-        @books = Book.all
+        @books = Book.all  
     end
-end
+    def show
+        @book = Book.find_by!(id: params[:id])
+    rescue ActiveRecord::RecordNotFound => e
+        Rails.logger.debug("Record not found")
+        redirect_to root_path
+        
+    end
+    def edit
+        @book = Book.find_by!(id: params[:id])
+    end
+    def update
+    
+        new_name = params[:book][:name]
+        @book = Book.find_by!(id: params[:id])
+        @book.update(name: new_name)
+        redirect_to book_path(@book)
+    end
+ end
